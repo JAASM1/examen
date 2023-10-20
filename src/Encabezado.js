3af49ac8bc87f0a995f52adbaa5cc4bb079ce9b2
joelrod12 <joelrodriguez128@gmail.com>
2023-10-20 09:45:04 -0500

    a

---
+++ src/Encabezado.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Encabezado() {
+    return (
+        <>
+            <header>
+                <h1>Lista de enfermedades de plantas</h1>
+            </header>
+        </>
+    )
+}
+
+// 4. Exportar el modulo
+export default Encabezado;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <p>todos lo derechos reservados</p>
+            </footer>
+        </>
+    )
+}
+
+// 4. Exportar el modulo
+export default Footer;
