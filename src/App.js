3753d16c0cc7109591963868cd0a58b9274f28eb
joelrod12 <joelrodriguez128@gmail.com>
2023-10-20 09:59:55 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Encabezado from "./Encabezado";
+import Nav from "./Nav";
+import Footer from "./Footer";
+
 function App() {
   return (
+    <>
+    <Encabezado/>
+    <Nav/>
+    <Footer/>
     <div className="App">
       <h1 className=""></h1>
     </div>
+    </>
+
   );
 }
 
--- src/Footer.js
+++ src/Footer.js
@@ -10,5 +10,4 @@ function Footer() {
     )
 }
 
-// 4. Exportar el modulo
 export default Footer;
