c1a12b3e2fd4daa77daf0b21d5d30bd16c52ea4b
TotoHacker <totochucl@gmail.com>
2023-10-20 09:38:16 -0500

    Creacion de componentes

---
+++ src/Componentes/ApiLisPlant.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+function ListPlant(){
+    
+    return (
+        <>
+        <Header></Header>
+        <h1>Hola Mundo</h1>
+        <Footer></Footer>
+        </>
+    )
+}
+export default ListPlant;
